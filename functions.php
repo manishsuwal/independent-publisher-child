a7ca3778bc7cf3bc0e4adb56ec598bd69ea34811
Manish Suwal <enwil410@gmail.com>
2014-02-04 11:55:12 +0545

    Add Efficient Related Posts to the bottom of post in Further Reading section

--- functions.php
+++ functions.php
@@ -20,3 +20,16 @@
      return $tweet_text . '%20via%20@manishsuwal'; // Adds " via @manishsuwal" to end of post title
  }
  add_filter('indiepub_sharing_buttons_tweet_text', 'indiepub_sharing_buttons_tweet_text');
+
+
+/*
+  * Add Efficient Related Posts to the bottom of posts in 'Further Reading Section'
+  */
+  function independent_publisher_show_further_reading() {
+      if ( is_single() && function_exists( 'wp_related_posts' ) ) :
+          echo '<div id="further-reading">';
+          do_action( 'erp-show-related-posts', array( 'title' => 'Further Reading', 'num_to_display' => 5, 'no_rp_text' => 'No Related Posts Found' ) );
+          echo '</div>';
+      endif;
+  }
+  add_action( 'independent_publisher_before_post_bottom_tag_list', 'independent_publisher_show_further_reading', 10 );
